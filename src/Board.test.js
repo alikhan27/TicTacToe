363c95241a6279c88a4ec00ea879bd4be76eb4be
Alikhan <muhammedalikhan27@gmail.com>
2024-01-29 17:03:07 +0530

    Added the test to check the button state to be disabled on first render

--- src/Board.test.js
+++ src/Board.test.js
@@ -13,4 +13,12 @@ describe('Should create the Tic Tac Toe Board Game', () => {
         const squares = screen.getAllByTestId('column').length;
         expect(squares).toBe(9);
     })
+
+    it('Should have squares button to be in disabled on first render', () => {
+        render(<Board/>);
+        const squares = screen.getAllByTestId('column');
+        squares.forEach(square => {
+            expect(square).toHaveAttribute('disabled');
+        })
+    })
 })
